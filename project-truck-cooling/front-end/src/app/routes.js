b60774e232c9bab80ec8cf8f78d495e672ba7601
Prima Aji Akbar <prima.aji.tif23@polban.ac.id>
2024-09-18 13:44:30 +0700

    ka

--- project-truck-cooling/front-end/src/app/routes.js
+++ project-truck-cooling/front-end/src/app/routes.js
@@ -12,35 +12,72 @@ import materialRoutes from "app/views/material-kit/MaterialRoutes";
 // SESSION PAGES
 const NotFound = Loadable(lazy(() => import("app/views/sessions/NotFound")));
 const JwtLogin = Loadable(lazy(() => import("app/views/sessions/JwtLogin")));
-const JwtRegister = Loadable(lazy(() => import("app/views/sessions/JwtRegister")));
-const ForgotPassword = Loadable(lazy(() => import("app/views/sessions/ForgotPassword")));
+const JwtRegister = Loadable(
+  lazy(() => import("app/views/sessions/JwtRegister"))
+);
+const ForgotPassword = Loadable(
+  lazy(() => import("app/views/sessions/ForgotPassword"))
+);
 const OTP = Loadable(lazy(() => import("app/views/sessions/OTP")));
-const ResetPassword = Loadable(lazy(() => import("app/views/sessions/ResetPassword")));
-const PasswordSuccess = Loadable(lazy(() => import("app/views/sessions/PasswordSuccess")));
+const ResetPassword = Loadable(
+  lazy(() => import("app/views/sessions/ResetPassword"))
+);
+const PasswordSuccess = Loadable(
+  lazy(() => import("app/views/sessions/PasswordSuccess"))
+);
 // E-CHART PAGE
-const AppEchart = Loadable(lazy(() => import("app/views/charts/echarts/AppEchart")));
+const AppEchart = Loadable(
+  lazy(() => import("app/views/charts/echarts/AppEchart"))
+);
 
 // DASHBOARD PAGE CLIENT
 const Analytics = Loadable(lazy(() => import("app/views/dashboard/Analytics")));
 // DASHBOARD PAGE ADMIN
-const AnalyticsAdmin = Loadable(lazy(() => import("app/views/dashboard/Analytics(Admin)")));
+const AnalyticsAdmin = Loadable(
+  lazy(() => import("app/views/dashboard/Analytics(Admin)"))
+);
 
 // DASHBOARD PAGE
 const Dashboard = Loadable(lazy(() => import("app/views/dashboard/Analytics")));
 const Riwayat = Loadable(lazy(() => import("app/views/Riwayat/RiwayatAdmin")));
-const RiwayatClient = Loadable(lazy(() => import("app/views/Riwayat/RiwayatClient")));
-const Pengaturan = Loadable(lazy(() => import("app/views/Pengaturan/Pengaturan")));
+const RiwayatClient = Loadable(
+  lazy(() => import("app/views/Riwayat/RiwayatClient"))
+);
+const Pengaturan = Loadable(
+  lazy(() => import("app/views/Pengaturan/Pengaturan"))
+);
 const Layanan = Loadable(lazy(() => import("app/views/Layanan/Layanan")));
-const LayananCLient = Loadable(lazy(() => import("app/views/Layanan/LayananClient")));
+const LayananCLient = Loadable(
+  lazy(() => import("app/views/Layanan/LayananClient"))
+);
 const Laporan = Loadable(lazy(() => import("app/views/Laporan/Laporan")));
-const Kelola_Client = Loadable(lazy(() => import("app/views/Kelola_Client/Kelola_Client")));
-const Kelola_Alat = Loadable(lazy(() => import("app/views/Kelola_Alat/Kelola_Alat")));
+const Kelola_Client = Loadable(
+  lazy(() => import("app/views/Kelola_Client/Kelola_Client"))
+);
+const Kelola_Alat = Loadable(
+  lazy(() => import("app/views/Kelola_Alat/Kelola_Alat"))
+);
 
-const Kelola_Komoditas_Client = Loadable(lazy(() => import("app/views/Kelola_Komoditas_Client/Kelola_Komoditas_Client")));
-const Informasi_Layanan = Loadable(lazy(() => import("app/views/Informasi_Layanan/Informasi_Layanan")));
-const Kelola_Alat_Client = Loadable(lazy(() => import("app/views/Kelola_Alat/Kelola_Alat_Client")));
-const Laporan_Client = Loadable(lazy(() => import("app/views/Laporan/Laporan_Client")));
-const Ubah_Profil = Loadable(lazy(() => import("app/views/Ubah_Profil/Ubah_Profil")));
+const Kelola_Komoditas_Client = Loadable(
+  lazy(() =>
+    import("app/views/Kelola_Komoditas_Client/Kelola_Komoditas_Client")
+  )
+);
+const Informasi_Layanan = Loadable(
+  lazy(() => import("app/views/Informasi_Layanan/Informasi_Layanan"))
+);
+const konfigurasiAlat = Loadable(
+  lazy(() => import("app/views/konfigurasi/konfigurasiAlat"))
+);
+const Kelola_Alat_Client = Loadable(
+  lazy(() => import("app/views/Kelola_Alat/Kelola_Alat_Client"))
+);
+const Laporan_Client = Loadable(
+  lazy(() => import("app/views/Laporan/Laporan_Client"))
+);
+const Ubah_Profil = Loadable(
+  lazy(() => import("app/views/Ubah_Profil/Ubah_Profil"))
+);
 
 const routes = [
   {
@@ -53,30 +90,91 @@ const routes = [
       ...materialRoutes,
 
       //View Klien
-      { path: "/Layanan/client", element: <LayananCLient />, auth: authRoles.client },
-      { path: "/Riwayat/client", element: <RiwayatClient />, auth: authRoles.client },
-      { path: "/KelolaKomoditas/client", element: <Kelola_Komoditas_Client />, auth: authRoles.client },
-      { path: "InformasiLayanan/client", element: <Informasi_Layanan />, auth: authRoles.client },
-      { path: "/Kelola_Alat/client", element: <Kelola_Alat_Client />, auth: authRoles.client },
-      { path: "/Laporan/client", element: <Laporan_Client />, auth: authRoles.client },
-      { path: "/Ubah_Profil/client", element: <Ubah_Profil />, auth: authRoles.client },
+      {
+        path: "/Layanan/client",
+        element: <LayananCLient />,
+        auth: authRoles.client,
+      },
+      {
+        path: "/konfigurasi/client",
+        element: <konfigurasi />,
+        auth: authRoles.client,
+      },
+      {
+        path: "/Riwayat/client",
+        element: <RiwayatClient />,
+        auth: authRoles.client,
+      },
+      {
+        path: "/KelolaKomoditas/client",
+        element: <Kelola_Komoditas_Client />,
+        auth: authRoles.client,
+      },
+      {
+        path: "InformasiLayanan/client",
+        element: <Informasi_Layanan />,
+        auth: authRoles.client,
+      },
+      {
+        path: "/Kelola_Alat/client",
+        element: <Kelola_Alat_Client />,
+        auth: authRoles.client,
+      },
+      {
+        path: "/Laporan/client",
+        element: <Laporan_Client />,
+        auth: authRoles.client,
+      },
+      {
+        path: "/Ubah_Profil/client",
+        element: <Ubah_Profil />,
+        auth: authRoles.client,
+      },
 
       // dashboard route
-      { path: "/dashboard/client", element: <Analytics />, auth: authRoles.client },
+      {
+        path: "/dashboard/client",
+        element: <Analytics />,
+        auth: authRoles.client,
+      },
       // dashboard route untuk admin
-      { path: "/dashboard/admin", element: <AnalyticsAdmin />, auth: authRoles.admin },
+      {
+        path: "/dashboard/admin",
+        element: <AnalyticsAdmin />,
+        auth: authRoles.admin,
+      },
 
-      { path: "/dashboard/default", element: <Dashboard />, auth: authRoles.admin },
+      {
+        path: "/dashboard/default",
+        element: <Dashboard />,
+        auth: authRoles.admin,
+      },
       { path: "/Riwayat/admin", element: <Riwayat />, auth: authRoles.admin },
-      { path: "/Kelola_Alat/admin", element: <Kelola_Alat />, auth: authRoles.admin },
-      { path: "/Kelola_Client/admin", element: <Kelola_Client />, auth: authRoles.admin },
+      {
+        path: "/Kelola_Alat/admin",
+        element: <Kelola_Alat />,
+        auth: authRoles.admin,
+      },
+      {
+        path: "/Kelola_Client/admin",
+        element: <Kelola_Client />,
+        auth: authRoles.admin,
+      },
       { path: "/Layanan/admin", element: <Layanan />, auth: authRoles.admin },
       { path: "/Laporan/admin", element: <Laporan />, auth: authRoles.admin },
-      { path: "/Pengaturan/admin", element: <Pengaturan />, auth: authRoles.admin },
+      {
+        path: "/Pengaturan/admin",
+        element: <Pengaturan />,
+        auth: authRoles.admin,
+      },
 
       // e-chart route
-      { path: "/charts/echarts", element: <AppEchart />, auth: authRoles.editor }
-    ]
+      {
+        path: "/charts/echarts",
+        element: <AppEchart />,
+        auth: authRoles.editor,
+      },
+    ],
   },
 
   // session pages route
@@ -88,9 +186,8 @@ const routes = [
   { path: "/session/ResetPassword", element: <ResetPassword /> },
   { path: "/session/PasswordSuccess", element: <PasswordSuccess /> },
 
-
   { path: "/", element: <Navigate to="dashboard/client" /> },
-  { path: "*", element: <NotFound /> }
+  { path: "*", element: <NotFound /> },
 ];
 
 export default routes;
