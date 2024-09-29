0c7633bff79de287238460f44b759508bfd7ba11
Prima Aji Akbar <prima.aji.tif23@polban.ac.id>
2024-09-29 18:03:19 +0700

    add stats

--- project-truck-cooling/back-end/index.js
+++ project-truck-cooling/back-end/index.js
@@ -1,38 +1,46 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const bodyParser = require('body-parser')
-const pool = require('./src/models/admin')
-const response = require('./src/res/response')
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+const pool = require("./src/models/admin");
+const response = require("./src/res/response");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/',(req,res) =>{
-  const sql = "SELECT * from alat"
-  pool.query(sql , (error, result) => {
-      response(200, result, "get all data from alat", res)
-  })
+app.get("/", (req, res) => {
+  const sql = "SELECT * from alat";
+  pool.query(sql, (error, result) => {
+    response(200, result, "get all data from alat", res);
+  });
   // console.log({urlparams : req.query})
-})
+});
 
-app.get('/find', (req, res) => {
+app.get("/find", (req, res) => {
   // console.log('find id: ', req.query.id)
-  const sql = `SELECT status FROM alat WHERE id = ${req.query.id}`
-  pool.query(sql, (error, result)=> {
-    response(200, result, "Find Name of alat", res)
-  })
-})
+  const sql = `SELECT status FROM alat WHERE id = ${req.query.id}`;
+  pool.query(sql, (error, result) => {
+    response(200, result, "Find Name of alat", res);
+  });
+});
 
-app.post('/login',(req,res) =>{
-    console.log({RequestFroOutside: req.body})
-    res.send("berhasil login")
-})
+app.post("/login", (req, res) => {
+  console.log({ RequestFroOutside: req.body });
+  res.send("berhasil login");
+});
 
-app.put('/username', (req, res) => {
-  console.log({UpdateData : req.body})
-  res.send("Update berhasil!!!")
-})
+app.put("/username", (req, res) => {
+  console.log({ UpdateData: req.body });
+  res.send("Update berhasil!!!");
+});
 
 app.listen(port, () => {
-    console.log(`anda masuk kedalam port ${port}`)
-})
+  console.log(`anda masuk kedalam port ${port}`);
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log("Database berhasil ditemukan");
+  }
+});
