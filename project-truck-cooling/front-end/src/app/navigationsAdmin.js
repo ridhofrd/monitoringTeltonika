736b05fe42f38db30e46cfaa77c782ae2a4566b9
Prima Aji Akbar <prima.aji.tif23@polban.ac.id>
2024-10-02 21:49:00 +0700

    hapus yang tidak perlu

--- project-truck-cooling/front-end/src/app/navigationsAdmin.js
+++ project-truck-cooling/front-end/src/app/navigationsAdmin.js
@@ -4,7 +4,7 @@ export const navigationsAdmin = [
   {
     name: "Riwayat",
     path: "/Riwayat/admin",
-    icon: "speaker_notes"
+    icon: "speaker_notes",
     // badge: { value: "30+", color: "secondary" },
     // children: [
     //   { name: "Auto Complete", path: "/material/autocomplete", iconText: "A" }
@@ -13,29 +13,28 @@ export const navigationsAdmin = [
   {
     name: "Kelola Alat",
     path: "/Kelola_Alat/admin",
-    icon: "shopping_cart"
+    icon: "shopping_cart",
   },
   {
     name: "Kelola Klien",
     path: "/Kelola_Client/admin",
-    icon: "perm_device_information"
+    icon: "perm_device_information",
   },
   {
     name: "Layanan",
     path: "/Layanan/admin",
-    icon: "laptop-2"
+    icon: "laptop-2",
   },
   {
     name: "Laporan",
     path: "/Laporan/admin",
-    icon: "graphic_eq"
+    icon: "graphic_eq",
   },
   {
     name: "Pengaturan",
     path: "/Pengaturan/admin",
-    icon: "settings"
+    icon: "settings",
   },
-
 
   { label: "Components", type: "label" },
   {
@@ -47,7 +46,11 @@ export const navigationsAdmin = [
       { name: "Buttons", path: "/material/buttons", iconText: "B" },
       { name: "Checkbox", path: "/material/checkbox", iconText: "C" },
       { name: "Dialog", path: "/material/dialog", iconText: "D" },
-      { name: "Expansion Panel", path: "/material/expansion-panel", iconText: "E" },
+      {
+        name: "Expansion Panel",
+        path: "/material/expansion-panel",
+        iconText: "E",
+      },
       { name: "Form", path: "/material/form", iconText: "F" },
       { name: "Icons", path: "/material/icons", iconText: "I" },
       { name: "Menu", path: "/material/menu", iconText: "M" },
@@ -56,8 +59,8 @@ export const navigationsAdmin = [
       { name: "Switch", path: "/material/switch", iconText: "S" },
       { name: "Slider", path: "/material/slider", iconText: "S" },
       { name: "Snackbar", path: "/material/snackbar", iconText: "S" },
-      { name: "Table", path: "/material/table", iconText: "T" }
-    ]
+      { name: "Table", path: "/material/table", iconText: "T" },
+    ],
   },
   {
     name: "Session/Auth",
@@ -65,19 +68,17 @@ export const navigationsAdmin = [
     children: [
       { name: "Sign in", iconText: "SI", path: "/session/signin" },
       { name: "Sign up", iconText: "SU", path: "/session/signup" },
-      { name: "Forgot Password", iconText: "FP", path: "/session/forgot-password" },
-      { name: "Error", iconText: "404", path: "/session/404" }
-    ]
-  },
-  { 
-    name: "Charts",
-    icon: "trending_up",
-    children: [{ name: "Echarts", path: "/charts/echarts", iconText: "E" }]
+      {
+        name: "Forgot Password",
+        iconText: "FP",
+        path: "/session/forgot-password",
+      },
+      { name: "Error", iconText: "404", path: "/session/404" },
+    ],
   },
   {
-    name: "Documentation",
-    icon: "launch",
-    type: "extLink",
-    path: "http://demos.ui-lib.com/matx-react-doc/"
-  }
+    name: "Charts",
+    icon: "trending_up",
+    children: [{ name: "Echarts", path: "/charts/echarts", iconText: "E" }],
+  },
 ];
