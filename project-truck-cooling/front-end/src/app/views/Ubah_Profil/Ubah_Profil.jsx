a3f2fa35a7ab0824f2cd45e9f32a2f5fbed28b19
Ihsan Ertansa Azhar <ihsan.azhar41@gmail.com>
2024-10-15 06:20:08 +0700

    Update Ubah Profil Features

--- project-truck-cooling/front-end/src/app/views/Ubah_Profil/Ubah_Profil.jsx
+++ project-truck-cooling/front-end/src/app/views/Ubah_Profil/Ubah_Profil.jsx
@@ -1,41 +1,94 @@
 import { Fragment } from "react";
-import { Card, Grid, styled, useTheme } from "@mui/material";
-import RowCards from "./shared/RowCards";
-import StatCards from "./shared/StatCards";
-import Campaigns from "./shared/Campaigns";
-import StatCards2 from "./shared/StatCards2";
-import DoughnutChart from "./shared/Doughnut";
-import UpgradeCard from "./shared/UpgradeCard";
-import TopSellingTable from "./shared/TopSellingTable";
-
+import React, { useState } from "react";
+import {
+  Card,
+  Grid,
+  styled,
+  useTheme,
+  Stack,
+  Button,
+  Modal,
+  Box,
+  Typography,
+  TextField,
+  Autocomplete,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+  ButtonGroup,
+} from "@mui/material";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 // STYLED COMPONENTS
 const ContentBox = styled("div")(({ theme }) => ({
   margin: "30px",
   [theme.breakpoints.down("sm")]: { margin: "16px" }
 }));
 
-const Title = styled("span")(() => ({
-  fontSize: "1rem",
-  fontWeight: "500",
-  marginRight: ".5rem",
-  textTransform: "capitalize"
-}));
 
-const SubTitle = styled("span")(({ theme }) => ({
-  fontSize: "0.875rem",
-  color: theme.palette.text.secondary
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
+
+const Container = styled("div")(({ theme }) => ({
+  margin: "30px",
 }));
 
 const H4 = styled("h4")(({ theme }) => ({
-  fontSize: "1rem",
-  fontWeight: "500",
-  marginBottom: "16px",
+  fontSize: "1.2rem",
+  fontWeight: "1000",
+  marginBottom: "35px",
   textTransform: "capitalize",
-  color: theme.palette.text.secondary
+  color: theme.palette.text.secondary,
 }));
 
-export default function Analytics() {
+const H5 = styled("h5")(({ theme }) => ({
+  fontSize: "1.2rem",
+  fontWeight: "700",
+  marginBottom: "35px",
+  textTransform: "capitalize",
+  color: theme.palette.text.secondary,
+}));
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  height: 650,
+  transform: "translate(-50%, -50%)",
+  width: 1000,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
+
+function createData(nama, fotoProfil, email, nomorWA, passwordLama, passwordBaru) {
+  return { nama, fotoProfil, email, nomorWA, passwordLama, passwordBaru};
+}
+
+export default function Ubah_Profil() {
   const { palette } = useTheme();
+  const [open, setopen] = React.useState(false);
+  const handleOpen = () => setopen(true);
+  const handleClose = () => setopen(false);
+  const [namabarang, setnamabarang] = useState("");
+  const [statusSatuan, setStatusSatuan] = useState("");
+  const [date, setDate] = useState("");
+  const [inputValue, setinputvalue] = useState({ id: "", label: "" });
 
   return (
     <Fragment>
@@ -49,17 +102,139 @@ export default function Analytics() {
             <H4>Ubah Profil Pengguna</H4>
             {/* <RowCards /> */}
           </Grid>
-
-          <Grid item lg={4} md={4} sm={12} xs={12}>
-          <Card sx={{ px: 3, py: 2, mb: 3 }}>
-              <Title>Card</Title>
-              <SubTitle>Pake kalo dibutuhin di page tertentu</SubTitle>
-            </Card>
-
-            {/* <UpgradeCard /> */}
-            {/* <Campaigns /> */}
-          </Grid>
         </Grid>
+              <Stack spacing={2}>
+                <Stack direction="row" spacing={2} alignItems="center">
+
+                </Stack>
+
+                <Stack direction="row" spacing={2} alignItems="center">
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    components="h6"
+                    sx={{ minWidth: "150px", fontSize: "1rem" }}
+                  >
+                    Nama Lengkap
+                  </Typography>
+
+                  <TextField
+                    label="Nama Lengkap"
+                    variant="outlined"
+                    sx={{ width: 500 }}
+                  />
+                </Stack>
+                <Stack direction="row" spacing={2} alignItems="center">
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    components="h6"
+                    sx={{ minWidth: "150px", fontSize: "1rem" }}
+                  >
+                    Email
+                  </Typography>
+
+                  <TextField
+                    label="Email"
+                    variant="outlined"
+                    sx={{ width: 500 }}
+                  />
+                </Stack>
+                <Stack direction="row" spacing={2} alignItems="center">
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    components="h6"
+                    sx={{ minWidth: "150px", fontSize: "1rem" }}
+                  >
+                    Nomor Whatsapp
+                  </Typography>
+
+                  <TextField
+                    label="Nomor Whatsapp"
+                    variant="outlined"
+                    sx={{ width: 500 }}
+                  />
+                </Stack>
+                <Stack direction="row" spacing={2} alignItems="center">
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    components="h6"
+                    sx={{ minWidth: "150px", fontSize: "1rem" }}
+                  >
+                    Foto Profil
+                  </Typography>
+
+                  <Button
+                    components="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    startIcon={<CloudUploadIcon />}
+                    sx={{ width: 150, height: 50 }}
+                  >
+                    Upload Foto
+                    <VisuallyHiddenInput
+                      type="file"
+                      onChange={(e) => console.log(e.target.files)}
+                      multiple
+                    />
+                  </Button>
+                </Stack>
+                <br></br>
+                <H5>Ubah Password</H5>
+                <Stack direction="row" spacing={2} alignItems="center">
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    components="h6"
+                    sx={{ minWidth: "150px", fontSize: "1rem" }}
+                  >
+                    Password Lama
+                  </Typography>
+
+                  <TextField
+                    label="Password Lama"
+                    variant="outlined"
+                    sx={{ width: 500 }}
+                  />
+                </Stack>
+                <Stack direction="row" spacing={2} alignItems="center">
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    components="h6"
+                    sx={{ minWidth: "150px", fontSize: "1rem" }}
+                  >
+                    Password Baru
+                  </Typography>
+
+                  <TextField
+                    label="Password Baru"
+                    variant="outlined"
+                    sx={{ width: 500 }}
+                  />
+                </Stack>
+
+              </Stack>
+              <Stack
+                direction="row"
+                spacing={12}
+                sx={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginTop: 5,
+                }}
+              >
+                <Button variant="contained" color="error">
+                  Reset
+                </Button>
+                <Button variant="contained" color="success">
+                  Simpan
+                </Button>
+              </Stack>
+          
       </ContentBox>
     </Fragment>
   );
