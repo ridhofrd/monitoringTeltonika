35905d65401d28fdbff3a9aa4dbe31e1aa102d79
Naufal Asidiq <naufalasidiq150@gmail.com>
2024-10-23 15:45:35 +0700

    autentikasi

--- project-truck-cooling/back-end/hashPassword.js
+++ project-truck-cooling/back-end/hashPassword.js
@@ -1,12 +1,12 @@
 import bcrypt from 'bcrypt';
 
-const password = 'lala25'; // Ganti dengan password asli yang ingin kamu hash
-const saltRounds = 10;
+// Fungsi untuk hash password
+export const hashPassword = async (password) => {
+    const saltRounds = 10;
+    return await bcrypt.hash(password, saltRounds);
+};
 
-bcrypt.hash(password, saltRounds, (err, hash) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('Password Hashed:', hash); // Hasil hash akan ditampilkan di sini
-    }
-});
+// Fungsi untuk membandingkan password
+export const comparePassword = async (password, hashedPassword) => {
+    return await bcrypt.compare(password, hashedPassword);
+};
