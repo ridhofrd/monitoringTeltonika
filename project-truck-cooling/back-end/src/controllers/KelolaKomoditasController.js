3061ba639373340ea0729dc2dc4336706870543d
Ihsan Ertansa Azhar <ihsan.azhar41@gmail.com>
2024-10-30 14:26:20 +0700

    create method PUT on commodity

--- project-truck-cooling/back-end/src/controllers/KelolaKomoditasController.js
+++ project-truck-cooling/back-end/src/controllers/KelolaKomoditasController.js
@@ -21,3 +21,43 @@ export const getKelolaKomoditas = async (req, res) => {
       }
 };
 
+export const updateKomoditas = async (req, res) => {
+  try {
+    const { id_commodity } = req.params;
+    let { namabarang, descbarang, satuan, gambarbarang, stokbarang} = req.body;
+
+    // Validasi input
+    if (!namabarang || !descbarang || !satuan || !gambarbarang || !stokbarang) {
+      return response(400, null, "Data Komoditas tidak lengkap", res);
+    }
+
+    let gambarURL = gambarbarang;
+
+    if (gambarbarang && !gambarbarang.startsWith("http")) {
+      gambarURL = `${req.protocol}://${req.get("host")}/public/images/${gambarbarang}`;
+    }
+
+    // Logging data yang akan dikirim ke database
+    console.log('Parameter query:', [namabarang, descbarang, satuan, gambarURL, stokbarang]);
+
+    const sql = `UPDATE commodity SET namabarang = $1, descbarang = $2, satuan = $3, gambarbarang = $4, stokbarang = $5 WHERE id_commodity = $10 RETURNING *`;
+
+    const result = await pool.query(sql, [namabarang, descbarang, satuan, gambarURL, stokbarang]);
+
+    if (result.rowCount) {
+      const data = {
+        isSuccess: result.rowCount,
+        message: "Update Data Successfully",
+        data: result.rows[0]
+      };
+      response(200, data, "Update Data is Successfully", res);
+    } else {
+      response(404, null, `Komoditas dengan id_commodity = ${id_commodity} tidak ditemukan`, res);
+    }
+  } catch (err) {
+    console.error("Error updating komoditas:", err);
+    response(500, null, `Error updating komoditas: ${err.message}`, res);
+  }
+}
+
+
